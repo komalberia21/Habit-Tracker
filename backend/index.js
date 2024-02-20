69a7f4410b9453c345746b3afcf8f6eaa81c6cf0
komalberia21 <komal.beria21@gmail.com>
2024-02-20 16:03:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const corsOptions = {
     credentials: true,
   };
   
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/user", userRouter);
